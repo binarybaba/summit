c084c4a30f7b723a942b23b98d4d18d171f8e2e6
JLMadman <100523559+JLMadman@users.noreply.github.com>
2022-02-27 20:13:03 +0530

    Change2

--- src/components/asin.js
+++ src/components/asin.js
@@ -3,6 +3,7 @@ const countries = [
     'US',
     'CA',
     'MX',
+    'MX',
 
 ]
 
---
+++ src/components/caseids.js
@@ -0,0 +1,22 @@
+const countries = [
+    'NA',
+    'EU',
+    'FE',
+
+]
+
+export const caseids = () => {
+    return (
+        <div>
+            <form action="">
+            <div>textarea</div>
+            <div>
+                {countries.map(countryCode => (
+                    <input />
+                ))}
+            </div>
+            </form>
+            
+        </div>
+    )
+}
