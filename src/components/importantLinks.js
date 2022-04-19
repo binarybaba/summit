c8888c17760f8143d05667b38ae9cd1f5296e0db
JLMadman <100523559+JLMadman@users.noreply.github.com>
2022-04-19 14:19:14 +0530

    Test

--- src/components/importantLinks.js
+++ src/components/importantLinks.js
@@ -49,6 +49,7 @@ export const linkTable = [
     { name: "Test Buy", link: "https://share.amazon.com/sites/BADSUMMIT/_layouts/15/WopiFrame2.aspx?sourcedoc=%7b7802A4B7-3C07-44F6-BAB1-1F5752260497%7d&file=SUMMIT%20Test%20Buy%20-%20Council%20Reviewed_CM_Final.docx&action=default" },
     { name: "BA SIM (Sangeetha)", link: "https://sim.amazon.com/issues/create?template=b26f32fb-b25c-46ae-949a-d96d02628375" },
     { name: "Suggestions?", link: "https://survey.fieldsense.whs.amazon.dev/survey/preview/8bac67c5-1f06-4a05-a1cf-c92e69290e1d" }
+    { name: "Suggestions?", link: "https://survey.fieldsense.whs.amazon.dev/survey/preview/8bac67c5-1f06-4a05-a1cf-c92e69290e1d" }
   ],
   [
     { name: "AML Insight", link: "https://amlinsight.lexisnexis.com/" },
